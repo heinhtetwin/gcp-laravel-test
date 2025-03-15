9a552bdd831a4900d89ae5e9f4672a13bbcf5f5c
Hein Htet Win <heinhtetwin386@gmail.com>
2025-03-16 01:32:43 +0700

    test run

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = base64decode(env("GOOGLE_CREDENTIALS_B64"))
+  project = var.project_id
+  region  = var.region
 }
 
 # Network
